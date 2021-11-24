8e1eec724cda42e9b853f47eed9c0e0fabe4208f
GinoBoldrini27 <ginojoaquin18@hotmail.com>
2021-11-23 21:21:54 -0300

    feat:Add nosotros and contacto pages 😁

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET nosotros page */
+router.get('/nosotros', (req, res) => {
+  res.render('pages/nosotros', { title: 'Nosotros' });
+});
+
+/* GET contacto page */
+router.get('/contacto', (req, res) => {
+  res.render('pages/contacto', { title: 'Contacto' });
+});
+
+
 
 module.exports = router;
